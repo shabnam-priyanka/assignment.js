83be3369bfaff1140082e1768e8fdb0702a8bba8
shabnam-priyanka <66578322+shabnam-priyanka@users.noreply.github.com>
2020-07-16 21:33:52 -0400

    first commit

---
+++ assignment.js
@@ -0,0 +1,52 @@
+// feetToMile
+
+function feetToMile(feet){
+    var feet = inch / ;
+    return feet;
+}
+
+var result = inchToFeet(144);
+console.log(result)
+
+
+
+// WoodCalculator
+
+function WoodCalculator(){
+    var chairCount = chair * ;
+    var tableCount = table *;
+    var bedCount = bed * ;
+
+    return totalWood;
+
+}
+var WoodResult = WoodCalculator( , , );
+console.log(WoodCalculator);
+
+
+
+
+//BrickCalculator
+
+
+
+
+
+
+//tinyFriend
+
+
+function friendTiny(number){
+    var largest = number[0];
+    for (var i = 0; i < number.length; i++){
+        var currentNumber = numbers[i];
+        if(currentNumber > largest){
+            largest = currentNumber;
+        }
+    }
+    return largest;
+}
+
+var largestNumber = biggestNumber("nisha","bishti","sajjadul","lipi","kitis","priyanka")
+
+console.log(largestNumber);
